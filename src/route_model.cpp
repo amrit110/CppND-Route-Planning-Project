82580c450bd44bee81a0b49b8de4af8c8565c441
amrit110 <amrit110@gmail.com>
2019-05-08 21:12:47 +0200

    Create RouteModel Nodes

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -2,5 +2,9 @@
 #include <iostream>
 
 RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
-
+    int node_idx = 0;
+    for(Model::Node node : this->Nodes()) {
+        RouteModel::m_Nodes.push_back(RouteModel::Node(node_idx, this, node));
+        node_idx++;
+    }
 }
